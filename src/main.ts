4070d4ca2ac9155726ec04b7bdb77013cec79b94
byeolki <byeolki7007@gmail.com>
2026-02-05 11:21:08 +0900

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDoc = new DocumentBuilder()
+    .setTitle('SimVex API')
+    .setVersion('1.0')
+    .build();
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(app, swaggerDoc),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
