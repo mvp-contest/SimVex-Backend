693554407618fc418cfecb46f0ba0691d5e5f459
byeolki <byeolki7007@gmail.com>
2026-02-06 20:47:12 +0900

    fix: add double quotes to message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "You won't die, because I'll protect you. - Ayanami Rei";
+    return '"You won\'t die, because I\'ll protect you." - Ayanami Rei';
   }
 }
